d5edc4c25d12f7de50007005c1be0f864fa27f69
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-25 15:39:44 +1200

    ADD: Final comments to API

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-// Allows for the creation of a user
+// Allows for the creation of a user and setting of best time/score
 
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
@@ -11,6 +11,7 @@ const prisma = new PrismaClient()
  */
 const createUser = async (req, res) => {
     try {
+        //Check request format
         const contentType = req.headers["content-type"];
         if (contentType !== "application/json") {
             return res.status(400).json({
@@ -19,6 +20,7 @@ const createUser = async (req, res) => {
             });
         }
 
+        //Get name from request body
         const { username } = req.body;
 
         // Check if user with same username already exists
@@ -107,32 +109,43 @@ const getUserByID = async (req, res) => {
     }
 }
 
+/**
+ * Checks for a users Unity score and adds/updates value if correct format
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} The response object with status and message
+ */
 const updateUserScore = async (req, res) => {
     try {
+        //Get ID and score values from request
         const userID = parseInt(req.params.id);
         const { unityBestScore } = req.body;
 
+        //If score is incorrect format
         if (unityBestScore === undefined) {
             return res.status(400).json({ error: "Score is required" });
         }
 
+        //Find the specified user
         const existingUser = await prisma.user.findUnique({
             where: { id: userID }
         });
 
+        //Return message if ID doesnt link to correct user stored in DB
         if (!existingUser) {
             return res.status(404).json({ error: "User not found" });
         }
 
+        //Set score values to find highest score
         const currentScore = existingUser.unityBestScore || 0; // Default to 0 if no score exists
         const newScore = parseInt(unityBestScore);
-
+        //Compare the two scores and set highest score for user
+        //Dont update if score lower than previous best
         if (newScore > currentScore) {
             const updatedUser = await prisma.user.update({
                 where: { id: userID },
                 data: { unityBestScore: newScore }
             });
-
             return res.status(200).json({ msg: "Score updated successfully", data: updatedUser });
         } else {
             return res.status(200).json({ msg: "Score not updated. New score is not higher." });
@@ -143,26 +156,39 @@ const updateUserScore = async (req, res) => {
     }
 };
 
+/**
+ * Checks for a users UE5 best time and adds/updates value if correct format
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} The response object with status and message
+ */
 const updateUserBestTime = async (req, res) => {
     try {
+        //Get ID and time values from request
         const userID = parseInt(req.params.id);
         const { unrealBestTime } = req.body;
 
+        //If time is incorrect format
         if (unrealBestTime === undefined) {
             return res.status(400).json({ error: "Best time is required" });
         }
 
+        //Find the specified user
         const existingUser = await prisma.user.findUnique({
             where: { id: userID }
         });
 
+        //Return message if ID doesnt link to correct user stored in DB
         if (!existingUser) {
             return res.status(404).json({ error: "User not found" });
         }
 
-        const currentBestTime = existingUser.unrealBestTime || Infinity; // Default to Infinity if no time exists
+        //Set time values to find highest score
+        const currentBestTime = existingUser.unrealBestTime || 0; // Default to Infinity if no time exists
         const newBestTime = parseFloat(unrealBestTime);
 
+        //Compare the two times and set lowest time for user
+        //Dont update if time higher than previous best
         if (newBestTime < currentBestTime) {
             const updatedUser = await prisma.user.update({
                 where: { id: userID },
--- routes/user.js
+++ routes/user.js
@@ -8,11 +8,11 @@ import {
 } from '../controllers/user.js';
 
 const router = express.Router();
-
-router.post('/', createUser); // /api/user
-router.get('/', getAllUsers); // /api/user
-router.get('/:id', getUserByID); // /api/user/1
-router.put('/score/:id', updateUserScore); // /api/user/score/1
-router.put('/besttime/:id', updateUserBestTime); // /api/user/besttime/1
+                                                    // Example endpoints
+router.post('/', createUser);                       // /api/user
+router.get('/', getAllUsers);                       // /api/user
+router.get('/:id', getUserByID);                    // /api/user/1
+router.put('/score/:id', updateUserScore);          // /api/user/score/1
+router.put('/besttime/:id', updateUserBestTime);    // /api/user/besttime/1
 
 export default router
